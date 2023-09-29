4a32dbf31d573654be7976d46f8cb35a6ecb7600
ishowdarkside <ajdin.omerovicc.ets@gmail.com>
2023-09-29 12:06:05 +0200

    added create folder panel

--- frontend/src/features/sidebar/Sidebar.jsx
+++ frontend/src/features/sidebar/Sidebar.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import AddFolder from "./AddFolder/AddFolder";
 import Folders from "./Folders/Folders";
 import styles from "./Sidebar.module.scss";
+import CreateFolderPanel from "./CreateFolderPanel/CreateFolderPanel";
 
 export default function Sidebar() {
+  const [isOpenPanel, setIsOpenPanel] = useState(false);
   return (
     <aside className={styles.sidebar}>
-      <AddFolder />
-      <Folders />
+      {!isOpenPanel && (
+        <>
+          <AddFolder onOpenPanel={setIsOpenPanel} />
+          <Folders />
+        </>
+      )}
+      {isOpenPanel && <CreateFolderPanel onClosePanel={setIsOpenPanel} />}
     </aside>
   );
 }
