b1d925bb157c969cfc47649be4a28ba7f4ffe6b1
Havock48 <adit.github@gmail.com>
2023-09-30 10:17:10 +0200

    sidebar ui finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import FolderLayout from "./features/sidebar/FolderLayout/FolderLayout";
 function App() {
   return (
     <BrowserRouter>
---
+++ frontend/src/features/sidebar/ImageCard/ImageCard.jsx
@@ -0,0 +1,7 @@
+import styles from './ImageCard.module.scss';
+export default function ImageCard({ data }) {
+	return (
+		<div className={styles.image}>
+		</div>
+	);
+}
