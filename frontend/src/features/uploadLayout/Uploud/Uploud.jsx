ac237d1e579f32dc516138def0438f39672cbcdc
Havock48 <adit.github@gmail.com>
2023-10-03 15:06:24 +0200

    uploud interface created

--- frontend/src/features/uploadLayout/Uploud/Uploud.jsx
+++ frontend/src/features/uploadLayout/Uploud/Uploud.jsx
@@ -1,9 +1,24 @@
+import { GrGallery } from 'react-icons/gr';
 import styles from './Uploud.module.scss';
 export default function Uploud() {
     return (
         <form>
             <label htmlFor='uploud' className={styles.uploud}>
-                <h2>Uploud your product image to get started</h2>
+                <div className={styles.header}>
+                    <GrGallery />
+                    <h2>Uploud your product image to get started</h2>
+                    <p><span>Click to uploud</span> or drag and drop</p>
+                    <p>Formats: JPG, PNG</p>
+                </div>
+                <div className={styles.footer}>
+                    <div className={styles.text}>
+                        <p>Remove background</p>
+                        <p>Product images with transparent backgrounds give the best result</p>
+                    </div>
+                    <div className="checkbox">
+                        <input type="checkbox" />
+                    </div>
+                </div>
             </label>
             <input type="file" id='uploud' className={styles.hide_input} />
         </form>
