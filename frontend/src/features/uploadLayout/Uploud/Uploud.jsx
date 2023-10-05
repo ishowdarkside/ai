3653dfaaf4b9cbcf77455f15db1b0c7741bc0616
Havock48 <adit.github@gmail.com>
2023-10-05 23:28:28 +0200

    drag and drop file uploud added

--- frontend/src/features/uploadLayout/Uploud/Uploud.jsx
+++ frontend/src/features/uploadLayout/Uploud/Uploud.jsx
@@ -13,9 +13,24 @@ export default function Uploud() {
     };
   }
 
+  function handleFileDrag(e) {
+    e.preventDefault();
+  } 
+  function handleFileDrop(e) {
+    e.preventDefault();
+    
+    setFile(e.dataTransfer.files[0])
+    const reader = new FileReader();
+    reader.readAsDataURL(e.dataTransfer.files[0]);
+    reader.onload = () => {
+      setImage(reader.result);
+    };
+  }
+
   return (
     <form className={styles.form}>
-      <label htmlFor="uploud" className={styles.uploud}>
+      <label htmlFor="uploud" className={styles.uploud} onDragOver={handleFileDrag}
+        onDrop={handleFileDrop}>
         <GrGallery />
         <h2>Uploud your product image to get started</h2>
         <p>
