f9e4f45c3fc3e30bd025a482d4c41f221a5876ac
Havock48 <adit.github@gmail.com>
2023-10-06 19:41:47 +0200

    images are saved on my products after generation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,58 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-import Home from "./pages/Home/Home";
-import FileContext from "./context/fileContext";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import FolderContext from "./context/FolderContext";
-import AiPrompt from "./pages/AiPrompt/AiPrompt";
-import PromptContext from "./context/PromptContext";
-import MyImages from "./pages/MyImages/MyImages";
-import ModalContext from "./context/ModalContext";
-import NotFound from "./pages/NotFound/NotFound";
-import MyProducts from "./pages/MyProducts/MyProducts";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Home from './pages/Home/Home';
+import FileContext from './context/fileContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import FolderContext from './context/FolderContext';
+import AiPrompt from './pages/AiPrompt/AiPrompt';
+import PromptContext from './context/PromptContext';
+import MyImages from './pages/MyImages/MyImages';
+import ModalContext from './context/ModalContext';
+import NotFound from './pages/NotFound/NotFound';
+import MyProducts from './pages/MyProducts/MyProducts';
 function App() {
-  const queryClient = new QueryClient();
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <ModalContext>
-                <FileContext>
-                  <FolderContext>
-                    <Home />
-                  </FolderContext>
-                </FileContext>
-              </ModalContext>
-            }
-          />
-          <Route
-            path="/ai-prompt"
-            element={
-              <PromptContext>
-                <AiPrompt />
-              </PromptContext>
-            }
-          />
-          <Route path="/myImages" element={<MyImages />} />
-          <Route path="/myProducts" element={<MyProducts />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <Toaster />
-    </QueryClientProvider>
-  );
+	const queryClient = new QueryClient();
+	return (
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path="/"
+						element={
+							<ModalContext>
+								<FileContext>
+									<FolderContext>
+										<Home />
+									</FolderContext>
+								</FileContext>
+							</ModalContext>
+						}
+					/>
+					<Route
+						path="/ai-prompt"
+						element={
+							<PromptContext>
+								<AiPrompt />
+							</PromptContext>
+						}
+					/>
+					<Route
+						path="/myImages"
+						element={
+							<ModalContext>
+								<MyImages />
+							</ModalContext>
+						}
+					/>
+					<Route path="/myProducts" element={<MyProducts />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+			<ReactQueryDevtools initialIsOpen={false} />
+			<Toaster />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
--- frontend/src/features/uploadLayout/Generator/Generator.jsx
+++ frontend/src/features/uploadLayout/Generator/Generator.jsx
@@ -1,152 +1,114 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import DraggableBox from "../DraggableBox/DraggableBox";
-import { useFileContext } from "../../../context/fileContext";
-import mergeImages from "merge-images";
-import { useEffect, useState } from "react";
-import { resizeProduct } from "../../../services/images";
-import { BsFillImageFill, BsArrowsMove } from "react-icons/bs";
-import { IoMdResize } from "react-icons/io";
-import styles from "./Generator.module.scss";
-import toast from "react-hot-toast";
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import DraggableBox from '../DraggableBox/DraggableBox';
+import { useFileContext } from '../../../context/fileContext';
+import mergeImages from 'merge-images';
+import { useEffect, useState } from 'react';
+import { resizeProduct } from '../../../services/images';
+import { BsFillImageFill, BsArrowsMove } from 'react-icons/bs';
+import { IoMdResize } from 'react-icons/io';
+import styles from './Generator.module.scss';
+import toast from 'react-hot-toast';
 
 export default function Generator() {
-  const {
-    image,
-    setFile,
-    file,
-    positions: { x, y },
-    selectedBackground,
-    boxRef,
-  } = useFileContext();
+	const { image, setFile, file, positions: { x, y }, selectedBackground, boxRef } = useFileContext();
 
-  const [backgroundByte, setBackgroundByte] = useState(null);
-  const [processedProduct, setProcessedProduct] = useState(null);
-  const [isMoving, setIsMoving] = useState(true);
+	const [ backgroundByte, setBackgroundByte ] = useState(null);
+	const [ processedProduct, setProcessedProduct ] = useState(null);
+	const [ isMoving, setIsMoving ] = useState(true);
 
-  useEffect(() => {
-    if (!selectedBackground) return;
-    async function convertByte() {
-      const response = await fetch(
-        `http://127.0.0.1:3000/${selectedBackground}`
-      );
+	useEffect(
+		() => {
+			if (!selectedBackground) return;
+			async function convertByte() {
+				const response = await fetch(`http://127.0.0.1:3000/${selectedBackground}`);
 
-      const blob = await response.blob();
-      const reader = new FileReader();
-      reader.readAsDataURL(blob);
-      reader.onload = () => {
-        setBackgroundByte(reader.result);
-      };
-    }
-    convertByte();
-  }, [selectedBackground]);
-  async function handleCompose() {
-    const productWidth = boxRef.current.getBoundingClientRect().width;
-    const productHeight = boxRef.current.getBoundingClientRect().height;
+				const blob = await response.blob();
+				const reader = new FileReader();
+				reader.readAsDataURL(blob);
+				reader.onload = () => {
+					setBackgroundByte(reader.result);
+				};
+			}
+			convertByte();
+		},
+		[ selectedBackground ]
+	);
+	async function handleCompose() {
+		const productWidth = boxRef.current.getBoundingClientRect().width;
+		const productHeight = boxRef.current.getBoundingClientRect().height;
 
-    const formData = new FormData();
-    formData.append("product", file, file.name);
-    formData.append("width", productWidth);
-    formData.append("height", productHeight);
-    const resizedProduct = await resizeProduct(
-      formData,
-      productWidth,
-      productHeight
-    );
+		const formData = new FormData();
+		formData.append('product', file, file.name);
+		formData.append('width', productWidth);
+		formData.append('height', productHeight);
+		const resizedProduct = await resizeProduct(formData, productWidth, productHeight);
 
-    mergeImages([
-      backgroundByte,
-      { src: resizedProduct, x: x * 1.5, y: y * 1.5 },
-    ])
-      .then((b64) => setProcessedProduct(b64))
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  return (
-    <div className={styles.generator}>
-      <div className={styles.panelsWrapper}>
-        <div className={styles.promptWrapper}>
-          <button onClick={() => setFile(null)} className={styles.chooseFile}>
-            Choose product
-          </button>
-          <DndProvider backend={HTML5Backend}>
-            <div
-              className={styles.image}
-              style={
-                selectedBackground && {
-                  backgroundImage: `url(http://127.0.0.1:3000/${selectedBackground})`,
-                }
-              }
-            >
-              <DraggableBox
-                id="box1"
-                left={50}
-                top={50}
-                image={image}
-                isMoving={isMoving}
-              ></DraggableBox>
-            </div>
-          </DndProvider>
+		mergeImages([ backgroundByte, { src: resizedProduct, x: x * 1.5, y: y * 1.5 } ])
+			.then((b64) => {
+				const items = JSON.parse(sessionStorage.getItem('generatedImages')) || [];
 
-          <button
-            onClick={() => setIsMoving((curr) => !curr)}
-            className={styles.toggleBtn}
-          >
-            {isMoving ? (
-              <div className={styles.resizeIcon}>
-                <IoMdResize />
-              </div>
-            ) : (
-              <div className={styles.moveIcon}>
-                <BsArrowsMove />
-              </div>
-            )}
-          </button>
-          <button
-            disabled={selectedBackground ? false : true}
-            onClick={handleCompose}
-            className={styles.generateBtn}
-          >
-            Generate
-          </button>
-        </div>
-        {processedProduct ? (
-          <a
-            href={processedProduct}
-            download
-            className={styles.outputLink}
-            onClick={() => {
-              try {
-                const items =
-                  JSON.parse(sessionStorage.getItem("generatedImages")) || [];
+				console.log(items.length);
+				sessionStorage.setItem('generatedImages', JSON.stringify([ ...items, b64 ]));
+				setProcessedProduct(b64);
+			})
+			.catch((err) => {
+				// Ovdje se treba ubacit poruka i kad je pun storage
+				console.log(err);
+			});
+	}
+	return (
+		<div className={styles.generator}>
+			<div className={styles.panelsWrapper}>
+				<div className={styles.promptWrapper}>
+					<button onClick={() => setFile(null)} className={styles.chooseFile}>
+						Choose product
+					</button>
+					<DndProvider backend={HTML5Backend}>
+						<div
+							className={styles.image}
+							style={
+								selectedBackground && {
+									backgroundImage: `url(http://127.0.0.1:3000/${selectedBackground})`
+								}
+							}
+						>
+							<DraggableBox id="box1" left={50} top={50} image={image} isMoving={isMoving} />
+						</div>
+					</DndProvider>
 
-                console.log(items.length);
-                sessionStorage.setItem(
-                  "generatedImages",
-                  JSON.stringify([...items, processedProduct])
-                );
-              } catch (err) {
-                toast.error(
-                  "Browser storage exceeded. To store new images, please remove some images from storage"
-                );
-              }
-            }}
-          >
-            <div className={styles.outputOverlay}>
-              <span>CLICK TO DOWNLOAD IMAGE</span>
-            </div>
-            <div
-              style={{ backgroundImage: `url(${processedProduct})` }}
-              className={styles.outputWrapper}
-            ></div>
-          </a>
-        ) : (
-          <div className={styles.fillPlaceholder}>
-            <BsFillImageFill />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+					<button onClick={() => setIsMoving((curr) => !curr)} className={styles.toggleBtn}>
+						{isMoving ? (
+							<div className={styles.resizeIcon}>
+								<IoMdResize />
+							</div>
+						) : (
+							<div className={styles.moveIcon}>
+								<BsArrowsMove />
+							</div>
+						)}
+					</button>
+					<button
+						disabled={selectedBackground ? false : true}
+						onClick={handleCompose}
+						className={styles.generateBtn}
+					>
+						Generate
+					</button>
+				</div>
+				{processedProduct ? (
+					<a href={processedProduct} download className={styles.outputLink}>
+						<div className={styles.outputOverlay}>
+							<span>CLICK TO DOWNLOAD IMAGE</span>
+						</div>
+						<div style={{ backgroundImage: `url(${processedProduct})` }} className={styles.outputWrapper} />
+					</a>
+				) : (
+					<div className={styles.fillPlaceholder}>
+						<BsFillImageFill />
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
